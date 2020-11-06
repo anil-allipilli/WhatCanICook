ae6235c19ac632bd0a270cc99960c2a285d4f6a5
anil-allipilli <anilkmr458@gmail.com>
2020-11-06 05:42:53 +0530

    frontend, HomePageView , settings

--- recipe/views.py
+++ recipe/views.py
@@ -1,3 +1,7 @@
+from django.views.generic import View
 from django.shortcuts import render
 
-# Create your views here.
+
+class HomePageView(View):
+    def get(self, request):
+        return render(request, "index.html")
