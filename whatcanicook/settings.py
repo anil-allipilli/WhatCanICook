b9e7dc1d6dd18af08b7f0d0464f998d33d59ae1c
anil-allipilli <anilkmr458@gmail.com>
2020-11-08 04:10:22 +0530

    settings

--- whatcanicook/settings.py
+++ whatcanicook/settings.py
@@ -21,9 +21,9 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-with open(os.path.join(BASE_DIR, 'secret_key.txt')) as f:
-    SECRET_KEY = f.read().strip()
-# SECRET_KEY = os.environ['SECRET_KEY']
+# with open(os.path.join(BASE_DIR, 'secret_key.txt')) as f:
+#     SECRET_KEY = f.read().strip()
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
